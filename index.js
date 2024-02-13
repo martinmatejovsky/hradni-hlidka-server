e75124ed05e8f3367b76bd4f27969d364feea21a
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-02-13 15:13:03 +0100

    Socket initialization

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const { createServer } = require('node:http');
 const path = require("path");
+const { Server } = require('socket.io');
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "../hradni-hlidka/dist")));
 const gameLocationsRouter = require('./routes/game-locations');
-app.use('/api/game-locations', gameLocationsRouter);
+const gameRouter = require('./routes/game');
 
-app.listen(3002, () => {
+app.use('/api/game-locations', gameLocationsRouter);
+app.use('/api/game', gameRouter);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3002, () => {
     console.log("Listen on the port 3002...");
 });
