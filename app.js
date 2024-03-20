44e2b1c8d540773c7ca47e4ed336da815f139dc2
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-03-20 21:09:22 +0100

    moving socket.io connection to separate file

--- app.js
+++ app.js
@@ -1,48 +1,22 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
 const server = http.createServer(app); // Create an HTTP server using Express
-const io = new Server(
-    server, { cors: { origin: "http://localhost:3000" } });
 const cors = require('cors');
 const path = require("path");
 const port = 8080;
 const gameLocationsRouter = require('./routes/game-locations');
 const gameRouter = require('./routes/game');
 const frontendPath = process.env.FRONTEND_PATH || "../hradni-hlidka/dist";
+const initializeSocket = require('./controllers/socketIo');
 
 // Middleware
-app.use(express.static(path.join(__dirname, frontendPath)));
 app.use(cors());
 app.use(express.json());
+initializeSocket(server);
 app.use('/api/game-locations', gameLocationsRouter);
 app.use('/api/game', gameRouter);
-
-// SOCKET
-io.on('connection', (socket) => {
-    socket.on('joinGame', (payload) => {
-        socket.join(payload.gameId);
-        console.log('User joined room', payload.gameId);
-        const controllerFile = `./controllers/testGameController-${payload.gameId}.js`;
-
-        const { joinNewPlayer } = require(controllerFile);
-        const gameWithNewPlayer = joinNewPlayer(payload.player);
-        io.to(payload.gameId).emit('newPlayerJoined', gameWithNewPlayer)
-    });
-
-    socket.on('leaveGame', (payload, callback) => {
-        console.log('User removed from room');
-        const controllerFile = `./controllers/testGameController-${payload.gameId}.js`;
-        const { removePlayer } = require(controllerFile);
-
-        const gameWithoutPlayer = removePlayer(payload.player);
-        io.to(payload.gameId).emit('playerLeftGame', gameWithoutPlayer)
-        socket.leave(payload.gameId);
-
-        callback(); // This will trigger the resolve in the client's promise
-    });
-});
+app.use(express.static(path.join(__dirname, frontendPath)));
 
 server.listen(port, () => {
     console.log("Listen on the port", port, "...");
---
+++ controllers/socketIo.js
@@ -0,0 +1,32 @@
+const { Server } = require("socket.io");
+
+function initializeSocket(server) {
+    const io = new Server(
+        server, { cors: { origin: "http://localhost:3000" } });
+
+    io.on('connection', (socket) => {
+        socket.on('joinGame', (payload) => {
+            socket.join(payload.gameId);
+            console.log('User joined room', payload.gameId);
+            const controllerFile = `./controllers/testGameController-${payload.gameId}.js`;
+
+            const {joinNewPlayer} = require(controllerFile);
+            const gameWithNewPlayer = joinNewPlayer(payload.player);
+            io.to(payload.gameId).emit('newPlayerJoined', gameWithNewPlayer)
+        });
+
+        socket.on('leaveGame', (payload, callback) => {
+            console.log('User removed from room');
+            const controllerFile = `./controllers/testGameController-${payload.gameId}.js`;
+            const {removePlayer} = require(controllerFile);
+
+            const gameWithoutPlayer = removePlayer(payload.player);
+            io.to(payload.gameId).emit('playerLeftGame', gameWithoutPlayer)
+            socket.leave(payload.gameId);
+
+            callback(); // This will trigger the resolve in the client's promise
+        });
+    });
+}
+
+module.exports = initializeSocket;
