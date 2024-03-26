4813077c087b5c8835d075bd5ed846ab82a434dc
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-03-26 22:04:58 +0100

    updating game in interval

--- controllers/testGameController-1.js
+++ controllers/testGameController-1.js
@@ -1,5 +1,7 @@
 const { mockGameInstance } = require('../mock-data/gameInstance');
 let gameInstance = {...mockGameInstance}
+const gameUpdateInterval = 2000;
+let gameUpdateIntervalId = null;
 
 module.exports.getGameInstance
 
@@ -14,6 +16,11 @@ module.exports.joinNewPlayer = (player) => {
 
 module.exports.removePlayer = (player) => {
     gameInstance.players = gameInstance.players.filter((item) => item.key !== player.key);
+    if (gameInstance.players.length === 0) {
+        if (gameUpdateIntervalId) clearInterval(gameUpdateIntervalId);
+        // TODO: in real app it should delete whole javascript file, not just set state "ready"
+        gameInstance.gameState = 'ready';
+    }
     return gameInstance;
 }
 
@@ -21,5 +28,14 @@ module.exports.startGame = (req, res, io) => {
     const gameId = req.body.gameId;
 
     gameInstance.gameState = 'running';
-    io.to(gameId).emit('gameStarted', gameInstance); // Emit event to one room
+    io.to(gameId).emit('gameStarted', gameInstance);
+    updateGame(gameId, io);
+}
+
+function updateGame(gameId, io) {
+    clearInterval(gameUpdateIntervalId);
+
+    gameUpdateIntervalId = setInterval(() => {
+        io.to(gameId).emit('gameUpdated', gameInstance);
+    }, gameUpdateInterval);
 }
