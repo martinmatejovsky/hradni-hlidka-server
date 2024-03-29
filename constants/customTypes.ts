d9b73646230997db95f16f6a25ba93aa649518cd
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-03-29 23:42:58 +0100

    typescript types and project constants

---
+++ constants/customTypes.ts
@@ -0,0 +1,44 @@
+export type Coordinates = {
+    latitude: number | null,
+    longitude: number | null,
+}
+export type PlayerCoordinates = Coordinates & {accuracy: number | null}
+export type PlayerData = {
+    name: string,
+    key: string,
+    location: PlayerCoordinates,
+}
+export type GameState = "ready" | "running" | "won" | "lost"
+type PolygonType = "battleZone" | "smithy" | "barracks"
+export interface BasePolygon {
+    polygonName: string,
+    key: string,
+    polygonType: PolygonType,
+    cornerCoordinates: Coordinates[],
+}
+export interface GameLocation {
+    locationName: string,
+    key: string,
+    polygons: BasePolygon[],
+}
+export interface BattleZone {
+    zoneName: string,
+    key: string,
+    cornerCoordinates: Coordinates[],
+    conquered: boolean,
+    guardians: PlayerData[],
+    assembledInvaders: Invader[],
+    assaultLadder: (Invader | null)[],
+}
+export interface GameInstance {
+    id: string,
+    gameState: GameState,
+    gameLocation: GameLocation,
+    battleZones: BattleZone[],
+    players: PlayerData[],
+}
+export type InvaderType = "normal"
+export type Invader = {
+    type: InvaderType,
+    health: number,
+}
---
+++ constants/projectConstants.ts
@@ -0,0 +1,3 @@
+export const GAME_UPDATE_INTERVAL: number = 1000
+export const GAME_TEMPO: number = 1000
+export const LADDER_POSITIONS: number = 10
