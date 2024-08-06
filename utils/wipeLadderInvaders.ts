445a660ffea71ca53ca42e11273b6f0d8bed97aa
Martin Matejovsky <martin.matejovsky@jpower8.cz>
2024-08-06 12:51:31 +0200

    new calculation of damaging invaders on ladder

--- utils/wipeLadderInvaders.ts
+++ utils/wipeLadderInvaders.ts
@@ -4,25 +4,30 @@ export const wipeLadderInvaders = (zones: BattleZone[]): void => {
     zones.forEach((zone: BattleZone): void => {
         let defendersStrength = zone.guardians.length;
         const ladder = zone.assaultLadder.content;
+        let invadersOnLadder = zone.invaders.filter(invader => invader.ladderStep !== null);
 
-        // hit only invaders that are at least at second position of the ladder so that it is not as easy
-        for (let i = ladder.length - 1; i >= 1 && defendersStrength > 0; i--) {
-            const invader = ladder[i];
+        if (invadersOnLadder.length > 0) {
+            // write me function: if an invader is on ladder position 1 or more, than hit it according to defenders strength and lower defenders strength
+            invadersOnLadder.forEach(invader => {
+                if (invader.ladderStep! > 0) {
+                    const damageStrength = Math.min(defendersStrength, invader.health);
 
-            if (invader !== null) {
-                const damageStrength = Math.min(defendersStrength, invader.health);
+                    // Deal damage to invader
+                    invader.health -= damageStrength;
 
-                // Deal damage to invader
-                invader.health -= damageStrength;
+                    // Remove invader if health drops to 0
+                    if (invader.health <= 0) {
+                        ladder[invader.ladderStep!] = null;
 
-                // Remove invader if health drops to 0
-                if (invader.health <= 0) {
-                    ladder[i] = null;
+                        // remove invader from zone.invaders array
+                        const invaderIndex = zone.invaders.indexOf(invader);
+                        zone.invaders.splice(invaderIndex, 1);
+                    }
+
+                    // Reduce defenders strength by the damage dealt
+                    defendersStrength -= damageStrength;
                 }
-
-                // Reduce defenders strength by the damage dealt
-                defendersStrength -= damageStrength;
-            }
+            })
         }
     });
 }
