41171a5060c3c7368ad4503062d7c2ad850229d2
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-12-09 21:54:08 +0100

    installed rollup plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,22 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'app.ts',
   output: {
     file: 'dist/app.js',
-    format: 'cjs',
+    format: 'es',
   },
   plugins: [
     resolve(),
     commonjs(),
     typescript({
       tsconfig: './tsconfig.json'
+    }),
+    json({
+      compact: true
     }),
     copy({
       targets: [
