c77f0e64a1d0652865db8bf717683d291430a390
Martin Matejovsky <matejovsky.ma@volny.cz>
2025-05-20 20:01:32 +0200

    testing constant change

--- src/constants/projectConstants.ts
+++ src/constants/projectConstants.ts
@@ -1,6 +1,6 @@
 import type {GameInstance} from "./customTypes";
 
-export const GAME_UPDATE_INTERVAL: number = 5000
+export const GAME_UPDATE_INTERVAL: number = 30000
 export const EMPTY_GAME_INSTANCE: GameInstance = {
     id: '',
     gameState: 'none',
