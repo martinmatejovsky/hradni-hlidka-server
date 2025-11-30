f39d31cfbd1de3a3fede406c706bdf5080e1330e
Martin Matejovsky <matejovsky.ma@seznam.cz>
2025-11-30 18:19:54 +0100

    feat(archers) calculating archers shooting

--- src/utils/manageArcherOutpost.ts
+++ src/utils/manageArcherOutpost.ts
@@ -2,8 +2,8 @@ import { BattleZone, ArcherPhases, PlayerData } from '../constants/customTypes.j
 
 const randomCooldown = (): number => Math.floor(Math.random() * 1) + 1;
 const randomOrientation = () => ({
-    horizontal: Math.round(Math.random() * 360),
-    vertical: Math.round(Math.random() * 90),
+    horizontal: Math.round(Math.random() * 360), // alpha
+    vertical: Math.round(10 + Math.random() * 70), // beta limited to 10–80
 });
 
 export default function manageArcherOutpost(zones: BattleZone[], players: PlayerData[]): void {
@@ -12,45 +12,54 @@ export default function manageArcherOutpost(zones: BattleZone[], players: Player
         player.underArrowAttack = false;
     });
 
-    zones.forEach((zone: BattleZone): void => {
-        const archers = zone.archers;
+    // correct loop for real game
+    // zones.forEach((zone: BattleZone): void => {
 
-        switch (archers.phase) {
-            case ArcherPhases.reloading:
-                if (archers.phaseTimer > 0) {
-                    archers.phaseTimer--;
-                } else {
-                    archers.phase = ArcherPhases.aiming;
-                    archers.phaseTimer = 1;
-                }
-                break;
+    // testing situation
+    let zone = zones[0];
 
-            case ArcherPhases.aiming:
-                if (archers.phaseTimer > 0) {
-                    archers.phaseTimer--;
-                } else {
-                    archers.phase = ArcherPhases.shooting;
-                    archers.phaseTimer = 1;
+    const archers = zone.archers;
 
-                    // Mark all guardians as attacked
-                    zone.guardians.forEach((playerId) => {
-                        const player: PlayerData | undefined = players.find((p) => p.key === playerId);
-                        if (player) {
-                            player.underArrowAttack = true;
-                            player.arrowDefendTarget = randomOrientation();
-                        }
-                    });
-                }
-                break;
+    switch (archers.phase) {
+        case ArcherPhases.reloading:
+            if (archers.phaseTimer > 0) {
+                archers.phaseTimer--;
+            } else {
+                archers.phase = ArcherPhases.aiming;
+                archers.phaseTimer = 1;
+            }
+            break;
 
-            case ArcherPhases.shooting:
-                if (archers.phaseTimer > 0) {
-                    archers.phaseTimer--;
-                } else {
-                    archers.phase = ArcherPhases.reloading;
-                    archers.phaseTimer = randomCooldown();
-                }
-                break;
-        }
-    });
+        case ArcherPhases.aiming:
+            if (archers.phaseTimer > 0) {
+                archers.phaseTimer--;
+            } else {
+                archers.phase = ArcherPhases.shooting;
+                archers.phaseTimer = 1;
+
+                // correct loop for real game
+                // Mark all guardians as attacked
+                // zone.guardians.forEach((playerId) => {
+                //     const player: PlayerData | undefined = players.find((p) => p.key === playerId);
+
+                // testing situation
+                players.forEach((player: PlayerData): void => {
+                    if (player) {
+                        player.underArrowAttack = true;
+                        player.arrowDefendTarget = randomOrientation();
+                    }
+                });
+            }
+            break;
+
+        case ArcherPhases.shooting:
+            if (archers.phaseTimer > 0) {
+                archers.phaseTimer--;
+            } else {
+                archers.phase = ArcherPhases.reloading;
+                archers.phaseTimer = randomCooldown();
+            }
+            break;
+    }
+    // });
 }
