83bebe895fd0ad8068eda32c6f23eb826578c384
Martin Matejovsky <matejovsky.ma@seznam.cz>
2025-11-30 18:20:43 +0100

    feat(certificate) added certificate to run on https

--- app.ts
+++ app.ts
@@ -1,25 +1,30 @@
-import express from "express";
+import express, { urlencoded } from 'express';
 const app = express();
-import * as http from "node:http";
-const server = http.createServer(app);
-import cors from "cors"
-import path from "path"
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
+import path from 'path';
 import { fileURLToPath } from 'url';
-const port = process.env.PORT || 8080;
 import gameLocationsRouter from './src/routes/game-locations.js';
 import gameRouter from './src/routes/game.js';
-const frontendPath = process.env.FRONTEND_PATH || "../hradni-hlidka/dist";
-import initializeSocket from './src/controllers/socketIo.js'
+
+const key = fs.readFileSync('./certificates/192.168.1.101-key.pem');
+const cert = fs.readFileSync('./certificates/192.168.1.101.pem');
+const server = https.createServer({ key, cert }, app);
+const port = process.env.PORT || 8080;
+const frontendPath = process.env.FRONTEND_PATH || '../hradni-hlidka/dist';
+import initializeSocket from './src/controllers/socketIo.js';
 import { Request, Response, NextFunction } from 'express';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-}
-));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN || 'https://192.168.1.101:3000', // or 'http://localhost:3000'
+    }),
+);
 app.use(express.json());
 initializeSocket(server);
 app.use('/api/game-locations', gameLocationsRouter);
@@ -29,6 +34,8 @@ app.use('/api/game', (req: Request, res: Response, next: NextFunction) => {
 });
 app.use(express.static(path.join(__dirname, frontendPath)));
 
-server.listen(port, '0.0.0.0', () => {
-    console.log("Server listening on the port", port, "...");
+// to see IP of localhost computer run IPCONFIG
+// to run server on PROD environment I probably have to use hostname '0.0.0.0'
+server.listen(8080, '192.168.1.101', () => {
+    console.log('HTTPS Backend running...');
 });
