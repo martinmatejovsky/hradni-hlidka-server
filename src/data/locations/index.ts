10ad1da0371172960bbe4540afefe21456583b41
Martin Matejovsky <matejovsky.ma@seznam.cz>
2026-02-19 07:35:41 +0100

    feat (locations) refactor of files with game locations

---
+++ src/data/locations/index.ts
@@ -0,0 +1,8 @@
+import { loketSportovni } from './loketSportovni';
+import { loketVychodniHradba } from './loketVychodniHradba';
+import { GameLocation } from '../../constants/customTypes';
+
+export const gameLocations: GameLocation[] = [
+    loketSportovni,
+    loketVychodniHradba
+];
